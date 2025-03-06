dba7a13689c6b8fb5c717f29897f0a78502a12ba
mario-alexx <marioatoroh@gmail.com>
2025-03-06 16:17:06 -0300

    adding authController with jwt

---
+++ ProductAPI/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Services;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly JwtService _jwtService;
+
+        public AuthController(JwtService jwtService)
+        {
+            _jwtService = jwtService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            if (request.Username == "admin@admin" && request.Password == "password")
+            {
+                var token = _jwtService.GenerateToken(request.Username);
+                return Ok(new { Token = token });
+            }
+
+            return Unauthorized("Invalid credentials");
+        }
+
+    }
+
+    public class LoginRequest
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
